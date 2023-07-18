80e74c6b8880d3a0dd9f2b034f33325a881a6d17
theo duval <theoduval76@icloud.com>
2023-07-18 16:11:36 +0200

    Modifications apportés.

---
+++ HTML/traitement_formulaire.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta http-equiv="refresh" content="5; URL=/HTML/index.html">
+</head>
+<body>
+    <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les données du formulaire
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $email = $_POST["email"];
+    $telephone = $_POST["telephone"];
+    $demande = $_POST["demande"];
+
+    // Générer le nom du fichier
+    $date = date("Y-m-d-H-i-s");
+    $directory = "client/";
+    $filename = $directory . $date . ".txt";
+
+    //  données à enregistrer dans le fichier
+    $data = "Nom: " . $nom . "\n";
+    $data .= "Prénom: " . $prenom . "\n";
+    $data .= "Email: " . $email . "\n";
+    $data .= "Téléphone: " . $telephone . "\n";
+    $data .= "Demande: " . $demande . "\n";
+
+    // Enregistrement  dans un fichier
+    $file = fopen($filename, "w");
+    fwrite($file, $data);
+    fclose($file);
+
+    // Affiche un message de succès
+    echo "Le formulaire a été soumis avec succès.";
+}
+?>
+</body>
+</html>
